471eb2fe68a1f3dfc1936c5de30c480bf7b92b3c
akshaynandi2762 <gd2762@wayne.edu>
2026-01-14 09:55:44 +0530

    Third commit

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -14,20 +14,6 @@ AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-aks
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
-# define AESD_ASSIGNMENTS_BUILD_CMDS
-# 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
-# endef
-
-# # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
-# define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-# 	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/bin
-# 	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/bin
-# 	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/bin
-# 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
-# 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
-# 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
-# endef
-
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
 endef
